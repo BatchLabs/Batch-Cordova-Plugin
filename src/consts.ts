af1388e734bd850bbdeea32342b1a574d11ba735
Arnaud Barisain-Monrose <arnaud@batch.com>
2018-10-17 15:56:21 +0200

    event: add new consts

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,10 @@ export const Consts = {
   AttributeKeyRegexp: /^[a-zA-Z0-9_]{1,30}$/,
   AttributeStringMaxLength: 64,
   BatchPluginName: "Batch",
-  DevelopmentMode: true
+  DevelopmentMode: true,
+  EventDataMaxTags: 10,
+  EventDataMaxValues: 10,
+  EventDataStringMaxLength: 64
 };
 
 export default Consts;
