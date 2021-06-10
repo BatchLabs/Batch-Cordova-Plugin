e52cf7af2d6c4e7f0b7965707fa549308d1259bb
Arnaud Barisain-Monrose <abarisain@gmail.com>
2021-06-10 11:12:26 +0200

    iOS/Android: Bump plugin version

--- dist/src/ios/BatchCordovaPlugin.h
+++ dist/src/ios/BatchCordovaPlugin.h
@@ -11,7 +11,7 @@
 #import "BatchBridge.h"
 #import "BatchBridgeCallback.h"
 
-#define PluginVersion "Cordova/2.99.98"
+#define PluginVersion "Cordova/3.0.0"
 
 @interface BatchCordovaPlugin : CDVPlugin <BatchBridgeCallback, BatchLoggerDelegate, BatchMessagingDelegate>
 
