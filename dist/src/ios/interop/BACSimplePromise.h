5b69948940296140ea0943288645bf7767f70b3b
Arnaud Barisain-Monrose <abarisain@gmail.com>
2021-09-06 15:35:43 +0200

    iOS: Update promise implementation

--- dist/src/ios/interop/BACSimplePromise.h
+++ dist/src/ios/interop/BACSimplePromise.h
@@ -3,12 +3,26 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSUInteger, BACSimplePromiseStatus) {
+    BACSimplePromiseStatusPending,
+    BACSimplePromiseStatusResolved,
+    BACSimplePromiseStatusRejected,
+};
+
 @interface BACSimplePromise<ObjectType: NSObject*> : NSObject
+
+@property BACSimplePromiseStatus status;
 
 + (nonnull instancetype)resolved:(nullable ObjectType)value;
 
++ (nonnull instancetype)rejected:(nullable NSError*)error;
+
 - (void)resolve:(nullable ObjectType)value;
 
+- (void)reject:(nullable NSError*)error;
+
 - (void)then:(void (^_Nonnull)(ObjectType _Nullable value))thenBlock;
+
+- (void)catch:(void (^_Nonnull)(NSError* _Nullable ))catchBlock;
 
 @end
--- dist/src/ios/interop/BACSimplePromise.m
+++ dist/src/ios/interop/BACSimplePromise.m
@@ -4,22 +4,22 @@
 #import "BACSimplePromise.h"
 
 /**
- A simple Promise-like implementation that can only be resolved and is not thread-safe.
+ A simple Promise-like implementation that is not thread-safe.
  then can't mutate the value
- catch does not exist
  */
 @implementation BACSimplePromise
 {
-    BOOL _resolved;
-    NSObject *_resolvedValue;
+    BACSimplePromiseStatus _status;
+    NSObject *_resolvedValue; // NSError* if rejected
     NSMutableArray *_thenQueue;
+    void (^_catchBlock)(NSError*);
 }
 
 - (instancetype)init
 {
     self = [super init];
     if (self) {
-        _resolved = false;
+        _status = BACSimplePromiseStatusPending;
         _resolvedValue = nil;
         _thenQueue = [NSMutableArray new];
     }
@@ -33,33 +33,69 @@
     return promise;
 }
 
++ (nonnull instancetype)rejected:(nullable NSError*)error
+{
+    BACSimplePromise *promise = [BACSimplePromise new];
+    [promise reject:error];
+    return promise;
+}
+
 - (void)resolve:(nullable NSObject*)value
 {
     @synchronized(_thenQueue) {
-        if (_resolved) {
+        if (_status != BACSimplePromiseStatusPending) {
             return;
         }
         
-        _resolved = true;
+        _status = BACSimplePromiseStatusResolved;
         _resolvedValue = value;
         
         void (^thenBlock)(NSObject*);
         while ([_thenQueue count] > 0) {
             thenBlock = [_thenQueue objectAtIndex:0];
             [_thenQueue removeObjectAtIndex:0];
-            thenBlock(value);
+            if (thenBlock) {
+                thenBlock(value);
+            }
         }
+    }
+}
+
+- (void)reject:(nullable NSError*)error
+{
+    if (_status != BACSimplePromiseStatusPending) {
+        return;
+    }
+    
+    _status = BACSimplePromiseStatusRejected;
+    _resolvedValue = error;
+    
+    if (_catchBlock) {
+        _catchBlock(error);
     }
 }
 
 - (void)then:(void (^_Nonnull)(NSObject* _Nullable ))thenBlock
 {
     @synchronized(_thenQueue) {
-        if (_resolved) {
+        if (_status == BACSimplePromiseStatusResolved) {
             thenBlock(_resolvedValue);
-        } else {
+        } else if (_status == BACSimplePromiseStatusPending) {
             [_thenQueue addObject:thenBlock];
         }
+    }
+}
+
+- (void)catch:(void (^_Nonnull)(NSError* _Nullable ))catchBlock
+{
+    if (_status == BACSimplePromiseStatusRejected) {
+        if ([_resolvedValue isKindOfClass:[NSError class]]) {
+            catchBlock((NSError*)_resolvedValue);
+        } else {
+            catchBlock(nil);
+        }
+    } else if (_status == BACSimplePromiseStatusPending) {
+        _catchBlock = catchBlock;
     }
 }
 
