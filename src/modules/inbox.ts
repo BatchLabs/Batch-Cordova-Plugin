05ad6279fc2fa206366519201e87f9c583c6c45b
Arnaud Barisain-Monrose <arnaud@batch.com>
2021-09-10 15:21:07 +0200

    plugin: refactor inbox methods

--- src/modules/inbox.ts
+++ src/modules/inbox.ts
@@ -49,6 +49,7 @@ export class InboxModule implements BatchSDK.InboxModule {
     return fetcher;
   }
 
+  /**
   private handleFetchCallback(
     rawResponse: string,
     userCallback: (
@@ -91,70 +92,6 @@ export class InboxModule implements BatchSDK.InboxModule {
     });
 
     userCallback(undefined, notifications);
-  }
+  }*/
 
-  private parseBridgeNotification(notif: {
-    [key: string]: unknown;
-  }): BatchSDK.InboxNotification {
-    if (typeof notif !== "object" || notif === null) {
-      throw new Error("Raw notification is not an object");
-    }
-
-    const body = notif.body;
-    if (!isString(body)) {
-      throw new Error("An Inbox Notification must at least have a body");
-    }
-
-    const identifier = notif.identifier;
-    if (!isString(identifier)) {
-      throw new Error("An Inbox Notification must at least have an identifier");
-    }
-
-    const rawDate = notif.date; // ts in ms
-    if (!isNumber(rawDate)) {
-      throw new Error("An Inbox Notification must at least have a date");
-    }
-
-    const isUnread = notif.is_unread;
-    if (typeof isUnread !== "boolean") {
-      throw new Error("An Inbox Notification must at least have a read flag");
-    }
-
-    let source = notif.source;
-    if (!isNumber(source)) {
-      throw new Error("An Inbox Notification must at least have a source");
-    }
-    if (
-      source !== InboxNotificationSource.CAMPAIGN &&
-      source !== InboxNotificationSource.TRANSACTIONAL &&
-      source !== InboxNotificationSource.TRIGGER &&
-      source !== InboxNotificationSource.UNKNOWN
-    ) {
-      source = InboxNotificationSource.UNKNOWN;
-    }
-
-    const parsedNotif: BatchSDK.InboxNotification = {
-      body,
-      date: new Date(rawDate),
-      identifier,
-      isUnread,
-      payload: {},
-      source: source as InboxModule["NotificationSource"],
-    };
-
-    if (isString(notif.ios_attachment_url)) {
-      parsedNotif.iOSAttachmentURL = notif.ios_attachment_url;
-    }
-
-    // TODO: make sure it's uniform with batchPushReceived
-    if (typeof notif.payload === "object" && notif.payload !== null) {
-      parsedNotif.payload = notif.payload as { [key: string]: unknown };
-    }
-
-    if (isString(notif.title)) {
-      parsedNotif.title = notif.title;
-    }
-
-    return parsedNotif;
-  }
 }
--- src/modules/inbox/inboxFetcher.ts
+++ src/modules/inbox/inboxFetcher.ts
@@ -1,4 +1,6 @@
 import { BatchSDK } from "../../../types";
+import { isNumber, isString } from "../../helpers";
+import { InboxModule, InboxNotificationSource } from "../inbox";
 
 abstract class BatchInboxFetcherBaseImplementation
   implements BatchSDK.InboxFetcher {
@@ -26,6 +28,71 @@ abstract class BatchInboxFetcherBaseImplementation
 
   dispose(): void {
     throw new Error("Method not implemented.");
+  }
+
+  protected _parseBridgeNotification(notif: {
+    [key: string]: unknown;
+  }): BatchSDK.InboxNotification {
+    if (typeof notif !== "object" || notif === null) {
+      throw new Error("Raw notification is not an object");
+    }
+
+    const body = notif.body;
+    if (!isString(body)) {
+      throw new Error("An Inbox Notification must at least have a body");
+    }
+
+    const identifier = notif.identifier;
+    if (!isString(identifier)) {
+      throw new Error("An Inbox Notification must at least have an identifier");
+    }
+
+    const rawDate = notif.date; // ts in ms
+    if (!isNumber(rawDate)) {
+      throw new Error("An Inbox Notification must at least have a date");
+    }
+
+    const isUnread = notif.is_unread;
+    if (typeof isUnread !== "boolean") {
+      throw new Error("An Inbox Notification must at least have a read flag");
+    }
+
+    let source = notif.source;
+    if (!isNumber(source)) {
+      throw new Error("An Inbox Notification must at least have a source");
+    }
+    if (
+      source !== InboxNotificationSource.CAMPAIGN &&
+      source !== InboxNotificationSource.TRANSACTIONAL &&
+      source !== InboxNotificationSource.TRIGGER &&
+      source !== InboxNotificationSource.UNKNOWN
+    ) {
+      source = InboxNotificationSource.UNKNOWN;
+    }
+
+    const parsedNotif: BatchSDK.InboxNotification = {
+      body,
+      date: new Date(rawDate),
+      identifier,
+      isUnread,
+      payload: {},
+      source: source as InboxModule["NotificationSource"],
+    };
+
+    if (isString(notif.ios_attachment_url)) {
+      parsedNotif.iOSAttachmentURL = notif.ios_attachment_url;
+    }
+
+    // TODO: make sure it's uniform with batchPushReceived
+    if (typeof notif.payload === "object" && notif.payload !== null) {
+      parsedNotif.payload = notif.payload as { [key: string]: unknown };
+    }
+
+    if (isString(notif.title)) {
+      parsedNotif.title = notif.title;
+    }
+
+    return parsedNotif;
   }
 }
 
