5acdc42270c0e2b657912f46caf88ec341fe82a9
Arnaud Barisain-Monrose <abarisain@gmail.com>
2021-09-28 14:00:53 +0200

    plugin: Make attribute internal variables private

--- src/modules/user/userAttributes.ts
+++ src/modules/user/userAttributes.ts
@@ -2,47 +2,47 @@ import { BatchSDK } from "../../../types";
 import { BatchUserAttributeType } from "../user";
 
 export class BatchUserAttribute implements BatchSDK.BatchUserAttribute {
-  constructor(public type: BatchUserAttributeType, public value: unknown) {}
+  constructor(private _type: BatchUserAttributeType, private _value: unknown) {}
 
   getType(): BatchSDK.BatchUserAttributeType {
-    return this.type;
+    return this._type;
   }
 
   getValue(): unknown {
     // Dates are mutable so they should be copied
-    if (this.value instanceof Date) {
-      return new Date(this.value.getTime());
+    if (this._value instanceof Date) {
+      return new Date(this._value.getTime());
     }
-    return this.value;
+    return this._value;
   }
 
   getStringValue(): string | undefined {
-    if (this.type == BatchUserAttributeType.STRING) {
-      return this.value as string;
+    if (this._type == BatchUserAttributeType.STRING) {
+      return this._value as string;
     }
     return undefined;
   }
 
   getBooleanValue(): boolean | undefined {
-    if (this.type == BatchUserAttributeType.BOOLEAN) {
-      return this.value as boolean;
+    if (this._type == BatchUserAttributeType.BOOLEAN) {
+      return this._value as boolean;
     }
     return undefined;
   }
 
   getNumberValue(): number | undefined {
     if (
-      this.type == BatchUserAttributeType.INTEGER ||
-      this.type == BatchUserAttributeType.DOUBLE
+      this._type == BatchUserAttributeType.INTEGER ||
+      this._type == BatchUserAttributeType.DOUBLE
     ) {
-      return this.value as number;
+      return this._value as number;
     }
     return undefined;
   }
 
   getDateValue(): Date | undefined {
-    if (this.type == BatchUserAttributeType.DATE) {
-      return new Date((this.value as Date).getTime());
+    if (this._type == BatchUserAttributeType.DATE) {
+      return new Date((this._value as Date).getTime());
     }
     return undefined;
   }
