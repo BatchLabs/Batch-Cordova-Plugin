a227fdf29ea7feca6375d29fba92289ad6be0e38
Arnaud Barisain-Monrose <abarisain@gmail.com>
2022-04-04 19:59:50 +0200

    all: 5.2.0 version bump

--- dist/src/ios/BatchCordovaPlugin.h
+++ dist/src/ios/BatchCordovaPlugin.h
@@ -11,7 +11,7 @@
 #import "BatchBridge.h"
 #import "BatchBridgeCallback.h"
 
-#define PluginVersion "Cordova/5.1.0"
+#define PluginVersion "Cordova/5.2.0"
 
 @interface BatchCordovaPlugin : CDVPlugin <BatchBridgeCallback, BatchLoggerDelegate, BatchMessagingDelegate>
 
