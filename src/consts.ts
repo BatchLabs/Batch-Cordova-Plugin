450b14b696c2f8850f35d036d74a8ce52d6a2ba7
Arnaud Roland <arnaud.roland@batch.com>
2026-04-21 15:07:52 +0200

    profile: extends cep string limits

--- src/consts.ts
+++ src/consts.ts
@@ -1,10 +1,6 @@
 export const Consts = {
-  AttributeKeyRegexp: /^[a-zA-Z0-9_]{1,30}$/,
-  AttributeStringMaxLength: 64,
-  AttributeStringArrayMaxSize: 25,
   BatchPluginName: "Batch",
   DevelopmentMode: true,
-  EventDataStringMaxLength: 64,
 };
 
 export default Consts;
