7f60c831a32ef3d42ae856843fd1a40cf16d623f
Arnaud Roland <arnaud.roland@batch.com>
2026-07-03 14:42:00 +0200

    all: bump batch to 3.4

--- dist/Package.swift
+++ dist/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apache/cordova-ios.git", from: "8.1.0"),
-        .package(url: "https://github.com/BatchLabs/Batch-iOS-SDK.git", from: "3.3.0")
+        .package(url: "https://github.com/BatchLabs/Batch-iOS-SDK.git", from: "3.4.0")
     ],
     targets: [
         .target(
